1ed94a7be5c44548ad56811c5326dfbc5f72e897
Kaylie Shelton <kaylieshelton@Kaylies-MacBook-Pro-2.local>
2024-01-07 18:05:39 -0500

    returns compatible list, materials, and explanation hopefully

--- ideacomplement.py
+++ ideacomplement.py
@@ -2,41 +2,45 @@ from flask import Flask, request, jsonify
 import openai
 import os
 
-# Given as input 
-ideaid = 1
-# Retrieved from database
+
+# To Be Retrieved from database
 ideas = ["Uber", "...","..."]
 resources = ["water", "metal", "gas", "paper"]
 values = [[1, 0, 2, 1], [2, 2, 0, 1], [0, 1, 1, 2]]
 
-
-# Find idea with most complements
-idea = values[ideaid]
-max_index = ideaid
-length = len(values[0])
-max_count = 0
-
-for index, ideavals in enumerate(values):
-    count = 0
-    for i in range(length):
-        if (idea[i] == 2 and ideavals[i] == 0) or (idea[i] == 0 and ideavals[i] == 2):
-            count += 1
-    if count > max_count:
-        max_count = count
-        max_index = index
-
-
-# Get that idea
-given_idea = ideas[ideaid]
-compatible_idea = ideas[max_index]
-
-#prompt? copied format from values.py, see if it works
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = Flask(__name__)
 
-@app.route('/generate_values')
-def generate_values():
+@app.route('/generate_complement')
+def generate_complement(ideaid):
+    # Find idea with most complements
+    idea = values[ideaid]
+    max_index = ideaid
+    max_list = idea
+    length = len(values[0])
+    max_count = 0
+
+    for index, ideavals in enumerate(values):
+        count = 0
+        for i in range(length):
+            if (idea[i] == 2 and ideavals[i] == 0) or (idea[i] == 0 and ideavals[i] == 2):
+                count += 1
+        if count > max_count:
+            max_count = count
+            max_index = index
+            max_list = ideavals
+
+    complementary_materials = []
+    for i in range(length):
+        if (idea[i] == 2 and max_list[i] == 0) or (idea[i] == 0 and max_list[i] == 2):
+            complementary_materials.append(resources[i])
+
+    # Get that idea
+    given_idea = ideas[ideaid]
+    compatible_idea = ideas[max_index]
+
+    #OPENAI STUFF HAVEN"T TESTED YET
     message = request.args.get("message")
     response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
@@ -54,7 +58,7 @@ def generate_values():
         max_tokens=128
     )
 
-    return str(response.choices[0].message.content)
+    return compatible_idea, complementary_materials, str(response.choices[0].message.content)
 
 
 
