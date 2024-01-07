01fa659014458e671a852afabbcea8f14ae443ba
Noah Kim <noahk@andrew.cmu.edu>
2024-01-08 01:03:55 +0900

    Typos

--- utilitariancalc.py
+++ utilitariancalc.py
@@ -5,7 +5,7 @@ import copy
 # Get the list of vectors from the language model in a list called Vectors
 # Vectors = [v_1, v_2, v_3, ..., v_n]
 newVectors = []
-for vector in Vectors:
+for vector in originalVectors:
     for e in vector:
         e += 1
     w = vector[1:]
@@ -14,7 +14,7 @@ for vector in Vectors:
 # Then we want to search for a = (v_j + ... + v_k)/n such that the norm of a is minimized. Note that the "neutral" vectors is the vector of all 1s. 
 # In other words, find a subgroup of vectors such that adding them all together gives you something that is close to zero as possible. 
     
-# helper funtion
+# helper funtions
 def subgroupSum(vectorList):
     sum = []
     for vector in vectorList:
@@ -35,7 +35,7 @@ def findSomeSubgroup(vector):
             subgroup.add(v)
     return subgroup
     
-# We want all subgroups, so we'll have a set of sets
+# We want all subgroups, so we'll have a set of sets (to avoid duplicate subgroups)
 allSubgroups = set()
 for vector in newVectors:
     if len(findSomeSubgroup(vector)) > 1:
